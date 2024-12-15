9d703c8fd3e628b13b6fe6870b6344e834a2787c
gemy26 <ahmed.gamal.5511@icloud.com>
2024-12-15 23:12:39 +0200

    add libraries

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -1,1 +1,10 @@
-﻿
+﻿open System
+open System.Windows.Forms
+open System.Drawing
+
+// Contact type to hold contact details
+type Contact = {
+    Name: string
+    PhoneNumber: string
+    Email: string
+}
